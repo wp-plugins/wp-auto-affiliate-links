448767e3614b424a9d1665c10e55ba93287719e3
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 17:02:59 +0000

    removed some leftover debug code
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/tags/3.10.2@901834 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aal_install.php
@@ -0,0 +1,91 @@
+<?php
+
+//Installation of plugin
+function aal_install() {
+	global $wpdb; 
+	$table_name = $wpdb->prefix . "automated_links";
+	
+
+	//if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+
+	$sql = "CREATE TABLE " . $table_name . " (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  link text NOT NULL,
+	  keywords text,
+	  meta text,
+	  medium varchar(255),
+	  grup int(5),
+	  grup_desc varchar(255),
+	  PRIMARY KEY (id)
+	  );";
+    
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        $wpdb->last_error;
+       // die();
+	
+}
+
+
+function aal_admin_notice() {
+	
+	$aal_notice_dismissed = get_option('aal_option_dismissed5'); 
+	if(!$aal_notice_dismissed)
+	{
+    ?>
+    <div id="aal_notice_div" class="updated">
+        <p align="center"><?php _e( 'Update: Amazon links can be automatically added from amazon, you only have to <a href="'. admin_url() .'admin.php?page=aal_apimanagement">request an api key</a> and then <a href="'. admin_url() .'admin.php?page=clickbank">set-up amazon</a>. with your amazon ID and to select a category. This feature will be available for free for a limited time. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:;" onclick="return aalDismiss();" >Dismiss this notice</a>', 'wp-auto-affiliate-links' ); ?></p>
+    </div>
+    
+<script type="text/javascript">
+	function aalDismiss() {
+
+
+        var data = {action: 'aal_dismiss_notice'};
+        
+            jQuery.ajax({
+                    type: "POST",
+                    url: ajax_script.ajaxurl,
+                    data: data,
+                    cache: false,
+                    success: function(){
+                    jQuery("#aal_notice_div").slideUp('slow', function() {jQuery("#aal_notice_div").remove();});
+                                        }
+                });
+        	
+		
+		
+	}
+
+</script>    
+    
+    
+    <?php
+	}
+	
+}
+
+
+function aalDismissNotice() {
+	
+	
+		delete_option('aal_option_dismissed1');
+		delete_option('aal_option_dismissed2');
+		delete_option('aal_option_dismissed3');
+		delete_option('aal_option_dismissed4');
+		delete_option('aal_option_dismissed5');
+		add_option('aal_option_dismissed5',true);
+	
+	
+}
+
+
+add_action( 'admin_notices', 'aal_admin_notice' );
+add_action('wp_ajax_aal_dismiss_notice', 'aalDismissNotice');
+
+
+
+
+
+
+?>
---
+++ modules/amazon/amazon.php
@@ -0,0 +1,121 @@
+<?php
+
+
+$aalAmazon = new aalModule('amazon','Amazon Links');
+$aalModules[] = $aalAmazon;
+
+$aalAmazon->aalModuleHook('content','aalAmazonDisplay');
+
+
+add_action( 'admin_init', 'aal_amazon_register_settings' );
+
+
+function aal_amazon_register_settings() { 
+   register_setting( 'aal_amazon_settings', 'aal_amazonid' );
+   register_setting( 'aal_amazon_settings', 'aal_amazonapikey' );
+   register_setting( 'aal_amazon_settings', 'aal_amazonsecret' );
+   register_setting( 'aal_amazon_settings', 'aal_amazoncat' );
+   register_setting( 'aal_amazon_settings', 'aal_amazonactive' );
+}
+
+
+
+
+function aalAmazonDisplay() {
+	
+	$amazoncat = get_option('aal_amazoncat');
+
+	?>
+
+<script type="text/javascript">
+
+function aal_amazon_validate() {
+	
+		if(!document.aal_amazonform.aal_amazoncat.value) { alert("Please select a category"); return false; }
+		if(!document.aal_amazonform.aal_amazonid.value) { alert("Please add your amazon ID"); return false; }
+				
+	}
+
+jQuery(document).ready(function() {
+      jQuery("#aal_amazoncat").val("<? echo $amazoncat; ?>");	
+}); 
+
+	
+	</script>
+	
+	
+	
+<div class="wrap">  
+    <div class="icon32" id="icon-options-general"></div>  
+        
+        
+                <h2>Amazon Links</h2>
+                <br /><br />
+                Once you add your affiliate ID and activate amazon links, they will start to appear on your website. The manual links that you add will have priority.<br />
+                This feature will only work if you have set the API Key in the "API Key" menu.
+                <br /><br />
+                
+<div class="aal_general_settings">
+		<form method="post" action="options.php" name="aal_amazonform" onsubmit="return aal_amazon_validate();"> 
+<?php
+		settings_fields( 'aal_amazon_settings' );
+		do_settings_sections('aal_amazon_settings_display');
+		
+?>
+		<span class="aal_label">Amazon Affiliate ID:</span> <input type="text" name="aal_amazonid" value="<?php echo get_option('aal_amazonid'); ?>" /><br />
+	<span class="aal_label">Category: </span><select id="aal_amazoncat"  name="aal_amazoncat" ><option value="">-Select a cateogry-	</option>
+		<option value="Apparel">Apparel & Accessories</option>
+		<option value="Software">Appstore for Android</option>
+		<option value="HomeGarden">Arts, Crafts & Sewing</option>
+		<option value="Automotive">Automotive</option>
+		<option value="Baby">Baby</option>
+		<option value="Beauty">Beauty</option>
+		<option value="Books">Books</option>
+		<option value="Electronics">Car Toys</option>
+		<option value="Wireless">Cell Phones & Accessories</option>
+		<option value="VideoGames">Computer & Video Games</option>
+		<option value="Electronics">Electronics</option>
+		<option value="HomeGarden">Gifts & Wish Lists</option>
+		<option value="Grocery">Grocery & Gourmet Food</option>
+		<option value="HealthPersonalCare">Health & Personal Care</option>
+		<option value="Kitchen">Home & Kitchen</option>
+		<option value="Tools">Home Improvement</option>
+		<option value="Industrial">Industrial & Scientific</option>
+		<option value="Jewelry">Jewelry</option>
+		<option value="KindleStore">Kindle Store</option>
+		<option value="Kitchen">Kitchen & Housewares</option>
+		<option value="Magazines">Magazine Subscriptions</option>
+		<option value="DVD">Movies &amp; TV</option>
+		<option value="Music">Music</option>
+		<option value="MusicalInstruments">Musical Instruments</option>
+		<option value="OfficeProducts">Office Products</option>
+		<option value="HomeGarden">Outlet</option>
+		<option value="PetSupplies">Pet Supplies</option>
+		<option value="Shoes">Shoes</option>
+		<option value="Software">Software</option>
+		<option value="Software">Sports & Outdoors</option>
+		<option value="Tools">Tools & Hardware</option>
+		<option value="Toys">Toys and Games</option>
+		<option value="Books">Travel</option>
+		<option value="All">All Categories</option>
+	</select>
+	<br />
+		<span class="aal_label">Status: </span><select name="aal_amazonactive">
+			<option value="0" <?php if(get_option('aal_amazonactive')=='0') echo "selected"; ?> > Inactive</option>
+			<option value="1" <?php if(get_option('aal_amazonactive')=='1') echo "selected"; ?> >Active</option>
+		</select><br />
+
+
+
+
+<?php
+	submit_button('Save');
+	echo '</form></div>';
+	echo '</div>';
+
+}
+
+
+
+
+?>
