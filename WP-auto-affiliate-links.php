63e11ca6f0005a90be9964147d9416889116b4c7
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-28 16:43:26 +0000

    added a notice about pro features
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@904228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-auto-affiliate-links.php
+++ WP-auto-affiliate-links.php
@@ -4,7 +4,7 @@ Plugin Name: WP Auto Affiliate Links
 Plugin URI: http://autoaffiliatelinks.com
 Description: Auto add affiliate links to your blog content
 Author: Lucian Apostol
-Version: 3.10.3
+Version: 3.10.4
 Author URI: http://autoaffiliatelinks.com
 */
 
