7ebbe04c0e3ed9311b9aecef11174f5fe8e21d30
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-11 09:44:37 +0000

    Created a new page for displaying Automated generated links
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@930203 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/clickbank/clickbank.php
+++ modules/clickbank/clickbank.php
@@ -118,71 +118,17 @@ function aal_clickbank_validate() {
 	echo '</form></div>';
 	echo '</div>';
 
-
-
-
-if(get_option('aal_clickbankactive')) { 
-
-
-		$data = file_get_contents('http://autoaffiliatelinks.com/api/getlinks.php?apikey='. get_option('aal_apikey') );
-		$data = json_decode($data);
-
-
-		$links = $data->links;
-		$number = $data->number;
-		
-		//print_r($data);
 		
 
 ?>
 
 
-
-
-<h3>Generated links</h3> 
-<p>Links here will not appear right away. Someone has to access a post first before he gets linked. Then the links will be shown here too</p>
-<br /><br /><br />
-<div class="aal_link_list">
-	<div class="aal_link_item">
-
-		<div class="aal_post_link">
-			Post link
-		</div>
-		<div class="aal_key_link">
-			Keywords
-		</div>
-	</div>	
-<?php foreach($links as $link) { 
-
-		$keywords = json_decode($link->keywords);
-		//print_r($keys);
-		$kwlist = '';
-		foreach($keywords as $keyword) {
-			
-			$kwlist .= '<a href="'. $keyword->url .'">'. $keyword->key .'</a> ';		
-		
-		}
-
-?>
-
-	<div style="clear: both; "></div>
-	
-	<div class="aal_link_item">
-		<div class="aal_post_link">
-			<a href="<?php echo $link->url; ?>"><?php echo $link->url; ?></a>
-		</div>
-		<div class="aal_key_link">
-			<?php echo $kwlist; ?>
-		</div>
-	</div>
-	<div style="clear: both; "></div>
-<?php } ?>
 </div>
 
 
 
 
 <?php 
-	}
+	
 
 } ?>
