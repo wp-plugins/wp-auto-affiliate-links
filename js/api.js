052b02e39614fed02a9101352117fc7022ed8b3a
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-29 06:14:56 +0000

    Added support for commission junction links
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@1015713 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/api.js
+++ js/api.js
@@ -21,6 +21,7 @@ jQuery(document).ready(function() {
 			aal_clickbankactive = datadiv.getAttribute('data-clickbankactive');
 			aal_shareasaleid = datadiv.getAttribute('data-shareasaleid');
 			aal_shareasaleactive = datadiv.getAttribute('data-shareasaleactive');
+			aal_cjactive = datadiv.getAttribute('data-cjactive');
 			aal_aurl = datadiv.getAttribute('data-aurl');
 			// aal_content = datadiv.getAttribute('data-content');
 			aal_content = encodeURIComponent(document.getElementById('aalcontent_' + aal_divnumber).parentNode.innerHTML);
@@ -30,7 +31,7 @@ jQuery(document).ready(function() {
 			//console.log(aal_content);
 			
 			
-			aalapidata = {content: aal_content, apikey: aal_apikey, clickbankid: aal_clickbankid, clickbankcat: aal_clickbankcat,  clickbankgravity: aal_clickbankgravity, amazonid: aal_amazonid, amazoncat: aal_amazoncat, amazonlocal: aal_amazonlocal, amazonactive: aal_amazonactive, clickbankactive: aal_clickbankactive, shareasaleactive: aal_shareasaleactive, shareasaleid: aal_shareasaleid,  aurl: aal_aurl, notimes: aal_notimes};
+			aalapidata = {content: aal_content, apikey: aal_apikey, clickbankid: aal_clickbankid, clickbankcat: aal_clickbankcat,  clickbankgravity: aal_clickbankgravity, amazonid: aal_amazonid, amazoncat: aal_amazoncat, amazonlocal: aal_amazonlocal, amazonactive: aal_amazonactive, clickbankactive: aal_clickbankactive, shareasaleactive: aal_shareasaleactive, shareasaleid: aal_shareasaleid, cjactive: aal_cjactive, aurl: aal_aurl, notimes: aal_notimes};
 			//alert(aalapidata);
 
 		
