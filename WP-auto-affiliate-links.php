3405df6252b873bbfff4e5b78ea43396a1e51d2c
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 05:42:54 +0000

    Fixed exclude posts deletion problem
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@1124148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-auto-affiliate-links.php
+++ WP-auto-affiliate-links.php
@@ -4,7 +4,7 @@ Plugin Name: Auto Affiliate Links
 Plugin URI: http://autoaffiliatelinks.com
 Description: Auto add affiliate links to your blog content
 Author: Lucian Apostol
-Version: 4.9.6.1
+Version: 4.9.6.2
 Author URI: http://autoaffiliatelinks.com
 */
 
