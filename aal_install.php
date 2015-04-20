2b8e6c2fa6152daa453f52b21ff43e7dd91233e3
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 13:19:40 +0000

    Rearranged some javascript code
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@1139102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aal_install.php
+++ aal_install.php
@@ -38,42 +38,20 @@ function aal_install() {
 
 function aal_admin_notice() {
 	
-	$aal_notice_dismissed = get_option('aal_option_dismissed17'); 
+	$aal_notice_dismissed = get_option('aal_option_dismissed18'); 
 	if(!$aal_notice_dismissed && !get_option('aal_apikey') )
 	{ 
     ?>
     <div id="aal_notice_div" class="updated">
      <div style="float: right;padding-top: 10px;"><a href="javascript:;" onclick="return aalDismiss();" >Dismiss this notice</a></div>
-      <p><?php  _e( 'Amazon, Clickbank and Shareasale, Ebay, Walmart, Commission Junction, Bestbuy and Envato Marketplace  links can be automatically added into your content , you only have to <a href="http://autoaffiliatelinks.com/wp-login.php?action=register">get your API key</a>, add your affiliate ID and start earning. ', 'wp-auto-affiliate-links' ); 
+      <p><?php  // _e( 'Amazon, Clickbank and Shareasale, Ebay, Walmart, Commission Junction, Bestbuy and Envato Marketplace  links can be automatically added into your content , you only have to <a href="http://autoaffiliatelinks.com/wp-login.php?action=register">get your API key</a>, add your affiliate ID and start earning. ', 'wp-auto-affiliate-links' ); 
       
-// _e( 'Thank you for using Wp Auto Affiliate Links. To take advantage of all the plugin features, you need to go to our website and  <a href="http://autoaffiliatelinks.com/wp-login.php?action=register">get your API key</a>.', 'wp-auto-affiliate-links' );      
+ _e( 'Thank you for using Wp Auto Affiliate Links. To take advantage of all the plugin features, you need to go to our website and  <a href="http://autoaffiliatelinks.com/wp-login.php?action=register">get your API key</a>.', 'wp-auto-affiliate-links' );      
 
       
       ?></p>
      
-    </div>
-    
-<script type="text/javascript">
-	function aalDismiss() {
-
-
-        var data = {action: 'aal_dismiss_notice'};
-        
-            jQuery.ajax({
-                    type: "POST",
-                    url: ajax_script.ajaxurl,
-                    data: data,
-                    cache: false,
-                    success: function(){
-                    jQuery("#aal_notice_div").slideUp('slow', function() {jQuery("#aal_notice_div").remove();});
-                                        }
-                });
-        	
-		
-		
-	}
-
-</script>    
+    </div>  
     
     
     <?php
@@ -86,23 +64,10 @@ function aal_admin_notice() {
 function aalDismissNotice() {
 	
 	
-		delete_option('aal_option_dismissed1');
-		delete_option('aal_option_dismissed2');
-		delete_option('aal_option_dismissed3');
-		delete_option('aal_option_dismissed4');
-		delete_option('aal_option_dismissed5');
-		delete_option('aal_option_dismissed6');
-		delete_option('aal_option_dismissed7');
-		delete_option('aal_option_dismissed8');
-		delete_option('aal_option_dismissed9');
-		delete_option('aal_option_dismissed10');
-		delete_option('aal_option_dismissed11');
-		delete_option('aal_option_dismissed12');
-		delete_option('aal_option_dismissed13');
-		delete_option('aal_option_dismissed14');
 		delete_option('aal_option_dismissed15');
 		delete_option('aal_option_dismissed16');
-		add_option('aal_option_dismissed17',true);
+		delete_option('aal_option_dismissed17');
+		add_option('aal_option_dismissed18',true);
 	
 	
 }
--- readme.txt
+++ readme.txt
@@ -4,9 +4,9 @@ Donate link: http://autoaffiliatelinks.com
 Tags: affiliate, links, post, plugin, posts, url, keywords, text, content, automatic
 Requires at least: 2.5.2
 Tested up to: 4.1.1
-Stable tag: 4.9.7.7
+Stable tag: 4.9.7.8
 
-Automatically add affiliate links in your website content so you can make more money. You can specify the keywords and affiliate links you want to be added or you can let the plugin to automatically decide where to add links from available affiliate networks: Amazon, Clickbank, Ebay, Walmart, Shareasale, Commission Junction, Bestbuy or Envato.
+Automatically display affiliate links in your website content so you can make more money. You can specify the keywords and affiliate links you want to be added or you can let the plugin to automatically decide where to add links from available affiliate networks: Amazon, Clickbank, Ebay, Walmart, Shareasale, Commission Junction, Bestbuy or Envato Marketplace.
 
 == Description ==
 
@@ -106,8 +106,12 @@ If you can't find the options to do this, try LibreOffice, as it is a bit easier
 
 == Changelog ==
 
+= 4.9.7.8 =
+* Rearranged some javascript code
+* Removed unused leftover code
+
 = 4.9.7.7 =
-* Updated plugin instructions on mai page and getstarted text.
+* Updated plugin instructions on main page and getstarted text.
 
 = 4.9.7.6 =
 * Made keyword and url input fields bigger and responsive
