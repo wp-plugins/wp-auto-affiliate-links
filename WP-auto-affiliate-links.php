9c7293ecb35c44b13797111a7144e94030898a08
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 15:31:16 +0000

    Tested and adapted for Wordpress 4.1.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-auto-affiliate-links/trunk@1140834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-auto-affiliate-links.php
+++ WP-auto-affiliate-links.php
@@ -4,7 +4,7 @@ Plugin Name: Auto Affiliate Links
 Plugin URI: http://autoaffiliatelinks.com
 Description: Auto add affiliate links to your blog content
 Author: Lucian Apostol
-Version: 4.9.7.8
+Version: 4.9.7.9
 Author URI: http://autoaffiliatelinks.com
 */
 
